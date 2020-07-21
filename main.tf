93009925e8c41dbf93ca98063568cac72d529599
Satyabrata Mishra <45105711+satyabratamishra9@users.noreply.github.com>
2020-07-21 19:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "***"
-  secret_key = "***"
+  access_key = "****"
+  secret_key = "****"
 }
 
 module "ssh_key_pair" {
@@ -60,14 +60,13 @@ resource "aws_instance" "ec2-instance" {
   ami                    = "${data.aws_ami.amazon-ami.id}"
   instance_type          = "t2.medium"
   vpc_security_group_ids = ["${aws_security_group.security-group.id}"]
-  subnet_id              = "${floor(count.index / length(module.vpc.public_subnets))}"
+  subnet_id              = "${element(module.vpc.public_subnets, count.index % length(module.vpc.public_subnets))}"
   key_name               = "${aws_key_pair.ssh.key_name}"
 
   tags = {
     Name = "twitter-etl-demo-${count.index}"
   }
 }
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -75,11 +74,13 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/20", "10.0.2.0/20", "10.0.3.0/20"]
-  public_subnets  = ["10.0.101.0/20", "10.0.102.0/20", "10.0.103.0/20"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
+  enable_dns_hostnames = true
+
 }
 
 resource "aws_security_group" "security-group" {
@@ -87,17 +88,77 @@ resource "aws_security_group" "security-group" {
   vpc_id        = "${module.vpc.vpc_id}"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
   }
-  egress {
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 9092
+    to_port     = 9092
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 2181
+    to_port     = 2181
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 2888
+    to_port     = 2888
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 3888
+    to_port     = 3888
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 27017
+    to_port     = 27017
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 8080
+    to_port     = 8080
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 8081
+    to_port     = 8081
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 7077
+    to_port     = 7077
+  }
+  ingress {
+    protocol    = "tcp"
+    self        = true
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 65535
   }
+#  ingress {
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#    from_port   = 0
+#    to_port     = 0
+#  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
   tags = {
     Purpose = "twitter-etl-demo"
   }
@@ -113,7 +174,7 @@ resource "null_resource" "zookeeper_ip" {
   count = 3
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[count.index]} zookeeper_id=${count.index} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[count.index]} zookeeper_id=${count.index} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.zookeeper_hosts"]
@@ -131,7 +192,7 @@ resource "null_resource" "kafka_ip" {
   count = 3
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[count.index]} kafka_broker_id=${count.index} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[count.index]} kafka_broker_id=${count.index} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.kafka_hosts"]
@@ -147,7 +208,7 @@ resource "null_resource" "mongodb_master" {
 resource "null_resource" "mongodb_master_ip" {
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[3]} mongodb_master=True ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[3]} mongodb_master=True ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.mongodb_master"]
@@ -163,8 +224,8 @@ resource "null_resource" "mongodb_replicas" {
 resource "null_resource" "mongodb_replicas_ip" {
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[4]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
-              echo "${aws_instance.ec2-instance.*.public_ip[5]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[4]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[5]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.mongodb_replicas"]
@@ -180,8 +241,8 @@ resource "null_resource" "spark_master" {
 resource "null_resource" "spark_master_ip" {
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[6]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
-              echo "${aws_instance.ec2-instance.*.public_ip[7]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[6]} DNS=${aws_instance.ec2-instance.*.public_dns[6]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[7]} DNS=${aws_instance.ec2-instance.*.public_dns[7]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.spark_master"]
@@ -197,8 +258,8 @@ resource "null_resource" "spark_worker" {
 resource "null_resource" "spark_worker_ip" {
   provisioner "local-exec" {
     command = <<EOF
-              echo "${aws_instance.ec2-instance.*.public_ip[8]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
-              echo "${aws_instance.ec2-instance.*.public_ip[9]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[8]} DNS=${aws_instance.ec2-instance.*.public_dns[8]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
+              echo "${aws_instance.ec2-instance.*.public_ip[9]} DNS=${aws_instance.ec2-instance.*.public_dns[9]} ansible_ssh_private_key_file=secrets/twitter-etl-demo.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_python_interpreter=/usr/bin/python" >> hosts
     EOF
   }
   depends_on = ["null_resource.spark_worker"]
